5ec297d5544323c870c6bc0565ec06fb9cce2092
Gaber Elsayed <47481084+muradmohamed@users.noreply.github.com>
2021-10-14 15:22:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
